258a0375c113feb6bc9af372f8c18b24d0a125b5
Anang Kurniawan <anangk97@gmail.com>
2021-10-28 15:46:31 +0700

    fix(main): fix horizontal alignment in MainActivity.kt

--- app/src/main/java/com/anangkur/storybooknativeplayground/MainActivity.kt
+++ app/src/main/java/com/anangkur/storybooknativeplayground/MainActivity.kt
@@ -7,9 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
                     Column(
                         modifier = Modifier.fillMaxSize(),
                         verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
                         Text("hello world!")
                     }
